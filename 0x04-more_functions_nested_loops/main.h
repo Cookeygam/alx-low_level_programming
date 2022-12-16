79ce8c5e3c23f1525e787df073c019b57651575d
francis cookey <114502105+Cookeygam@users.noreply.github.com>
2022-12-16 04:10:13 +0100

    main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 
 int _putchar(char c);
 int _isupper(int c);
@@ -16,7 +17,4 @@ void print_triangle(int size);
 void print_number(int n);
 
 
-#endif 
-/* 
-Bwave ICT 
-*/
+#endif
