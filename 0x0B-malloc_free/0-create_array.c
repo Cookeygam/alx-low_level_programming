7deec0537c1a4fe4f3829a0cfcb8d3db8a8e34ec
cookeygam <cookeygam001@gmail.com>
2023-01-10 09:19:08 -0800

    thank you betty correct

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 /**
  * create_array - creates an array of chars.
  * @size: size of the array.
- * @c: storaged char
+ * @ch: storaged char
  *
  * Return: pointer of an array of chars
  */
@@ -11,12 +11,12 @@ char *create_array(unsigned int size, char ch)
 {
 	char *x;
 	unsigned int i;
-	
-  x = malloc(sizeof(ch) * size);
-	
+
+	x = malloc(sizeof(ch) * size);
+
 	if (size == 0 || x == NULL)
 		return (NULL);
-  
+
 	for (i = 0; i < size; i++)
 		x[i] = ch;
 
